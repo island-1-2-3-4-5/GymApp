aa3c9cd3b5e7fea6e840431555a30ba2ce65026e
Roman <r.monahov2014@yandex.ru>
2020-04-23 23:41:58 +0300

    Initial Commit

---
+++ GymApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GymApp
+//
+//  Created by Roman on 23.04.2020.
+//  Copyright © 2020 Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
